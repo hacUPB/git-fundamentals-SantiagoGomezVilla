3528536c5ea879afa6dbbca08acba7faf237c024
santiago <santiago.gomezvilla@upb.edu.co>
2024-02-02 18:36:11 -0500

    autoformato

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr,"Usage: program <number>\n");
-        return 1;
-    }
-    
-    int number = atoi(argv[1]); //Convert the console input argument to an integer
-    
-    /*It must be verified that the entered number is greater than or equal to zero*/
-    if(number < 0)
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
     {
-        fprintf(stderr,"The number must be greater than or equal to zero\n");
-        return(1);
+        fprintf(stderr, "Usage: program <number>\n");
+        return (1);
+    }
+
+    int number = atoi(argv[1]); // Convert the console input argument to an integer
+
+    /*It must be verified that the entered number is greater than or equal to zero*/
+    if (number < 0)
+    {
+        fprintf(stderr, "The number must be greater than or equal to zero\n");
+        return (1);
     }
 
     /*Implement the function that calculates the factorial of the input argument*/
@@ -23,29 +25,29 @@ int main(int argc, char *argv[]) {
     {
         factorial *= b;
     }
-    
+
     /*Print the result*/
-    printf("The factorial of %d is: %d\n",number,factorial);
+    printf("The factorial of %d is: %d\n", number, factorial);
 
     /*Implement the function that calculates if the argument is a prime number*/
-    int count = 0,index = 1;
+    int count = 0, index = 1;
     while (index < number)
     {
-        if(number%index == 0)
-            {
+        if (number % index == 0)
+        {
             count++;
-            }
+        }
         index++;
     }
-    
+
     /*Print the result*/
-    if(count == 1)
+    if (count == 1)
     {
-        printf("%d is not a prime number\n",number);
+        printf("%d is not a prime number\n", number);
     }
     else
     {
-        printf("%d is a prime number\n",number);
+        printf("%d is a prime number\n", number);
     }
     return (0);
 }
