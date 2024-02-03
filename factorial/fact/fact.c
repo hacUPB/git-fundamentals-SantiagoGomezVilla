b5908edf2614628a99efb46828936299fdbb306e
santiago <santiago.gomezvilla@upb.edu.co>
2024-02-02 20:21:32 -0500

    eliminacion espacios en numbers

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -27,10 +27,10 @@ int main(int argc, char *argv[])
     }
 
     /*Print the result*/
-    fprintf(stdout,"The factorial of %d is: %d\n", number, factorial);
+    fprintf(stdout,"The factorial of %d is: %d\n",number,factorial);
 
     /*Implement the function that calculates if the argument is a prime number*/
-    int count = 0, index = 1;
+    int count = 0,index = 1;
     while (index < number)
     {
         if (number%index == 0)
@@ -43,11 +43,11 @@ int main(int argc, char *argv[])
     /*Print the result*/
     if (count == 1)
     {
-        printf("%d is not a prime number\n", number);
+        printf("%d is not a prime number\n",number);
     }
     else
     {
-        printf("%d is a prime number\n", number);
+        printf("%d is a prime number\n",number);
     }
     return 0;
 }
